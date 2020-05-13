2e437f0dd27cf7c5394cfc714874b70905e08456
alfabot1337 <alfabot1337@gmail.com>
2020-05-13 08:54:05 +0300

    started product page

---
+++ work_5/product/js/main.js
@@ -0,0 +1,39 @@
+let convert = document.querySelector('.convert');
+if (scrolled > 20) {
+  convert.style.top = '90vh';
+} else if (scrolled < 20) {
+  convert.style.top = '620px';
+}
+
+function openBurger() {
+  document.querySelector('.burger').classList.toggle('burger_open');
+  document.querySelector('nav.fixed').classList.toggle('nav_open');
+}
+document.querySelector('.burger').addEventListener('click', openBurger);
+
+function searchToggle(e) {
+let searchInput = document.querySelector('.navbar__search');
+let searchButton = document.querySelector('.navbar__search + a');
+searchButton.classList.toggle('search-show');
+searchInput.classList.toggle('search-show');
+}
+
+function popUp() {
+let popup = document.querySelector('.popup-wrapper');
+popup.classList.toggle('show');
+}
+var backgrounds = [
+'./img/main.jpg',
+'img/main-bg-2.jpg',
+'img/main-bg-3.jpg'
+]
+var BACKGROUND_IMAGE_CHANGE_INTERVAL = 5 * 1000
+var currentImage = 0;
+(function showNextImage() {
+document.querySelector('header').style.backgroundImage = 'url(' + backgrounds[currentImage] + ')'
+currentImage += 1
+if (currentImage >= backgrounds.length) {
+  currentImage = 0
+}
+setTimeout(showNextImage, BACKGROUND_IMAGE_CHANGE_INTERVAL)
+}())
