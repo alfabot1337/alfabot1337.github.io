e0b31e0c8b4e418fc06b8a903a26ab92c9db5be7
Maxim Ostapenko <alfabot1337@gmail.com>
2020-05-14 21:26:45 +0300

    Product page done

--- work_5/product/js/main.js
+++ work_5/product/js/main.js
@@ -1,10 +1,3 @@
-let convert = document.querySelector('.convert');
-if (scrolled > 20) {
-  convert.style.top = '90vh';
-} else if (scrolled < 20) {
-  convert.style.top = '620px';
-}
-
 function openBurger() {
   document.querySelector('.burger').classList.toggle('burger_open');
   document.querySelector('nav.fixed').classList.toggle('nav_open');
@@ -22,18 +15,25 @@ function popUp() {
 let popup = document.querySelector('.popup-wrapper');
 popup.classList.toggle('show');
 }
-var backgrounds = [
-'./img/main.jpg',
-'img/main-bg-2.jpg',
-'img/main-bg-3.jpg'
-]
-var BACKGROUND_IMAGE_CHANGE_INTERVAL = 5 * 1000
-var currentImage = 0;
-(function showNextImage() {
-document.querySelector('header').style.backgroundImage = 'url(' + backgrounds[currentImage] + ')'
-currentImage += 1
-if (currentImage >= backgrounds.length) {
-  currentImage = 0
-}
-setTimeout(showNextImage, BACKGROUND_IMAGE_CHANGE_INTERVAL)
-}())
+// var backgrounds = [
+// './img/main.jpg',
+// 'img/main-bg-2.jpg',
+// 'img/main-bg-3.jpg'
+// ]
+// var BACKGROUND_IMAGE_CHANGE_INTERVAL = 5 * 1000
+// var currentImage = 0;
+// (function showNextImage() {
+// document.querySelector('header').style.backgroundImage = 'url(' + backgrounds[currentImage] + ')'
+// currentImage += 1
+// if (currentImage >= backgrounds.length) {
+//   currentImage = 0
+// }
+// setTimeout(showNextImage, BACKGROUND_IMAGE_CHANGE_INTERVAL)
+// }())
+
+let menuRight = document.querySelector('.fixed-right');
+menuRight.style.right = (window.outerWidth - 1140) / 2 + 'px';
+
+window.addEventListener('resize', () => {
+	menuRight.style.right = (window.outerWidth - 1140) / 2 + 'px';
+});
