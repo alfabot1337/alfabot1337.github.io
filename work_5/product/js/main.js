eb4d63d64b0e44bb50ebcc0ef4591c4a079ca2d3
Maxim Ostapenko <alfabot1337@gmail.com>
2020-05-15 20:05:11 +0300

    fixed work 5

--- work_5/product/js/main.js
+++ work_5/product/js/main.js
@@ -1,3 +1,68 @@
+let scrolled;
+window.onscroll = function () {
+  scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  let scrollBlock = document.querySelector('.header_scroll');
+  let navBarLinks = document.querySelectorAll('.navbar__item a');
+  let navBarActive = document.querySelector('.navbar___active a');
+  let [,...navBarItems] = document.querySelectorAll('.navbar__item');
+  let navBarLang = document.querySelector('.navbar__language');
+  let headerTop = document.querySelector('.header__top');
+  let logo = document.querySelector('.logo');
+  let secondLanguage = this.document.querySelector('.second-language');
+
+  if (scrolled > 400) {
+    scrollBlock.style.top = '0';
+    headerTop.style.top = '15px';
+    logo.src = '../img/A.I.K.svg';
+    navBarLinks.forEach((i) => (i.style.color = '#2d5ec2'));
+    navBarItems.forEach(i => i.style.borderLeft = '1px solid #2d5ec2');
+    navBarLang.style.border = '1px solid #2d5ec2';
+    secondLanguage.style.border = '1px solid #2d5ec2';
+    document.querySelector('nav.fixed').style.top = '15px';
+
+    if (window.innerWidth <= 1200 && window.innerWidth > 1024) {
+        document.querySelector('nav.fixed').style.top = '60px';
+        console.log('x');
+        
+    } else if (window.innerWidth <= 1024) {
+        headerTop.style.top = '20px';
+        document.querySelector('nav.fixed').style.top = '0';
+        navBarItems.forEach(i => i.style.borderLeft = 'none');
+        navBarLinks.forEach((i) => (i.style.color = '#fff'));
+        navBarActive.style.color = '#fff';
+        
+    }
+    
+  } else if (scrolled < 400) {
+    scrollBlock.style.top = '-200px';
+    headerTop.style.top = '50px';
+    logo.src = '../img/logo.png'
+    navBarLinks.forEach((i) => (i.style.color = '#fff'));
+    navBarItems.forEach(i => i.style.borderLeft = '1px solid #fff');
+    navBarLang.style.border = '1px solid #fff';
+    navBarActive.style.color = '#2d5ec2';
+    secondLanguage.style.border = '1px solid #fff';
+    document.querySelector('nav.fixed').style.top = '50px';
+    if (window.innerWidth <= 1200 && window.innerWidth > 1024) {
+        document.querySelector('nav.fixed').style.top = '95px';
+    }else if (window.innerWidth <= 1024) {
+        headerTop.style.top = '20px';
+        document.querySelector('nav.fixed').style.top = '0';
+        navBarItems.forEach(i => i.style.borderLeft = 'none');
+        navBarLinks.forEach((i) => (i.style.color = '#fff'));
+        navBarActive.style.color = '#fff';
+
+    }
+  }
+
+  let convert = document.querySelector('.convert');
+  if (scrolled > 20) {
+    convert.style.top = '90vh';
+  } else if (scrolled < 20) {
+    convert.style.top = '620px';
+  }
+};
+
 function openBurger() {
   document.querySelector('.burger').classList.toggle('burger_open');
   document.querySelector('.nav').classList.toggle('nav_open');
