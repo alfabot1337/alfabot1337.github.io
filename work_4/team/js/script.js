4e91d8872a8d16853e8730f7c845242d73bcb2f1
Maxim Ostapenko <alfabot1337@gmail.com>
2020-05-22 14:00:11 +0300

    Add blog, team, contact page at work 4

--- work_4/team/js/script.js
+++ work_4/team/js/script.js
@@ -3,40 +3,47 @@ const players = [
 		name: 'David Road',
 		spec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis',
 		linkedin: 'https://linkedin.com/alfabot1337',
-		instagram: 'https://instagram.com/maxxlwr'
+		instagram: 'https://instagram.com/maxxlwr',
+		image: 'img/player-1.png'
 	},
 	{
 		name: 'Maxim Ostapenko',
 		spec: '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis',
 		linkedin: 'https://linkedin.com/alfabot13371',
-		instagram: 'https://instagram.com/maxxlwr'
+		instagram: 'https://instagram.com/maxxlwr',
+		image: 'img/player-2.png'
 	},
 	{
 		name: 'Alyona Sarapina',
 		spec: '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis',
 		linkedin: '2https://linkedin.com/alfabot1337',
-		instagram: 'https://instagram.com/maxxlwr'
+		instagram: 'https://instagram.com/maxxlwr',
+		image: 'img/player-3.png'
 	},
 	{
 		name: 'Dmitriy Skrypchenko',
 		spec: '3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis',
 		linkedin: 'https://linkedin.com/alfabot1337',
-		instagram: 'https://instagram.com/maxxlwr'
+		instagram: 'https://instagram.com/maxxlwr',
+		image: 'img/player-4.png'
 	}
 ];
 
 
 const changeDesc = e => {
-	const playerId = e.target.id;
+	console.log(e)
+	const playerId = e.target.parentElement.id;
 	const playerName = document.querySelector('h4.description__heading');
 	const playerSpec = document.querySelector('p.description__spec');
 	const playerLinkInst = document.querySelector('.description__links .instagram');
 	const playerLinkLinkedIn = document.querySelector('.description__links .linkedin');
+	const sliderInfo = document.querySelector('.slider__main_info');
 
 	playerName.innerHTML = players[playerId].name;
 	playerSpec.innerHTML = players[playerId].spec;
 	playerLinkInst.href = players[playerId].instagram;
 	playerLinkLinkedIn.href = players[playerId].linkedin;
+	sliderInfo.style.backgroundImage = `url(${players[playerId].image})`;
 }
 
 const sliderItems = document.querySelectorAll('.slider__item');
